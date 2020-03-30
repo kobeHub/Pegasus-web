65a230b40f3ba44f83a75ea5ebdf1feea8340cd0
Inno Jia <jdgets111@gmail.com>
2020-03-30 22:17:45 +0800

    #bug: iframe option not fix

---
+++ src/components/kuber/NodeGrafana.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="nodeview">
+    <div class="col-12 ml-auto">
+      <b-card-group deck>
+        <b-card bg-variant="dark" header="Dark" text-variant="white" class="text-center">
+          <iframe src="http://grafana.local/d-solo/mNdiCfrWz/pegasus-front?orgId=1&refresh=10s&from=1585564422701&to=1585564722701&panelId=6" width="450" height="200" frameborder="0"></iframe>
+          <div>
+
+          </div>
+        </b-card>
+
+        <b-card bg-variant="dark" header="Dark" text-variant="white" class="text-center">
+          <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+        </b-card>
+      </b-card-group>
+    </div>
+    <div class="col-12 mt-3 ml-auto">
+      <b-card-group deck>
+        <b-card bg-variant="dark" header="Dark" text-variant="white" class="text-center">
+          <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+        </b-card>
+
+        <b-card bg-variant="dark" header="Dark" text-variant="white" class="text-center">
+          <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+        </b-card>
+      </b-card-group>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NodeGrafana',
+}
+</script>
+
+<scss scoped lang="scss">
+
+</scss>
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Login from '@/components/user/Login'
 import Register from '@/components/user/Register'
 import Invitation from '@/components/user/Invitation'
 import Department from '@/components/user/Department'
+import NodeGrafana from '@/components/kuber/NodeGrafana'
 import Error404 from '@/components/Errors/error-404'
 import Error500 from '@/components/Errors/error-500'
 
@@ -43,6 +44,10 @@ export default new Router({
         {
             path: '/departments',
             component: Department
+        },
+        {
+            path: '/node',
+            component: NodeGrafana,
         },
         {
             path: '/error',
